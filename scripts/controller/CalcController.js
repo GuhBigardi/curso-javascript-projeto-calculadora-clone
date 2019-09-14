748faf0ef37c9a7e098005bcc85ee1553e71584c
Gustavo Bigardi <guhbigardi@hotmail.com>
2019-09-14 00:03:21 -0300

    calculadora funcional

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -32,8 +32,73 @@ class CalcController {
         this._operation.pop()
     }
 
-    addOperation(value) {
+    getLastOperation() {
+        return this._operation[this._operation.length - 1]
+    }
+
+    isOperator(value) {
+        return (['/', '*', '-', '+', '%'].indexOf(value) > -1)
+    }
+
+    setLastOperation(value) {
+        this._operation[this._operation.length - 1] = value
+    }
+
+    pushOperation(value) {
         this._operation.push(value)
+        if (this._operation.length > 3) {
+            this.calc()
+        }
+    }
+
+    calc() {
+        let last = this._operation.pop()
+        let result = eval(this._operation.join(""))
+        this._operation = [result, last]
+        this.setLastNumberToDisplay()
+    }
+
+    setLastNumberToDisplay() {
+
+        let lastNumber;
+        for (let i = this._operation.length - 1; i >= 0; i--) {
+            if (!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i]
+                break
+            }
+        }
+
+        this.displayCalc = lastNumber
+    }
+
+    addOperation(value) {
+        let lastOperation = this.getLastOperation()
+
+        if (isNaN(lastOperation)) {
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value)
+
+            } else if (isNaN(value)) {
+
+
+            } else {
+
+                this.pushOperation(value)
+                this.setLastNumberToDisplay()
+            }
+
+        } else if (isNaN(value)) {
+
+            this.pushOperation(value)
+
+        } else {
+            let newValue = lastOperation.toString() + value.toString()
+            this.setLastOperation(parseInt(newValue))
+            this.setLastNumberToDisplay()
+        }
+
     }
 
     setError() {
@@ -50,14 +115,22 @@ class CalcController {
                 this.clearEntry()
                 break
             case 'soma':
+                this.addOperation('+')
                 break
             case 'subtracao':
+                this.addOperation('-')
                 break
             case 'divisao':
+                this.addOperation('/')
                 break
             case 'multiplicacao':
+                this.addOperation('*')
                 break
             case 'porcento':
+                this.addOperation('%')
+                break
+            case 'ponto':
+                this.addOperation('.')
                 break
             case 'igual':
                 break
